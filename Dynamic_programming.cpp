23dcb5011a009b3461f8a549574fd1099b01c4eb
Вероника Римашевская <Sultt.nika@yandex.ru>
2023-10-03 08:51:47 +0300

    Добавлен вывод списка вещей для лучшего решения

--- Dynamic_programming.cpp
+++ Dynamic_programming.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <unordered_map>
 #include <string>
 
@@ -7,6 +7,7 @@ using namespace std;
 
 int main() {
 
+    //список вещей для выбора
     unordered_map<string, unordered_map<int, int>>tableCost;
     tableCost.insert({ "Water", {{ 3,10 }} });
     tableCost.insert({ "Book", {{ 1,3 } } });
@@ -14,6 +15,7 @@ int main() {
     tableCost.insert({ "Cloth", {{ 2,5 } } });
     tableCost.insert({ "Camera", {{ 1,6 } } });
 
+    //размер сумки
     const int maxBag = 6;
 
     int** resTable = new int* [tableCost.size()];
@@ -23,6 +25,7 @@ int main() {
     for (int i = 0; i < tableCost.size(); i++)
         for (int j = 0; j < maxBag; j++) resTable[i][j] = 0;
     
+    string* listItem = new string [tableCost.size()];
 
 
     int i = 0;
@@ -31,7 +34,7 @@ int main() {
     cout << "|-------|--------------------------------------------------|\n";
     for (const auto& n : tableCost) {
         cout <<"|"<< n.first << "\t|  ";
-
+        listItem[i] = n.first;
         for (const auto& k : n.second) {
             for (int j = 0; j < maxBag; j++) {
                 if (i == 0) if (k.first <= (j + 1)) resTable[i][j] = k.second; 
@@ -48,6 +51,17 @@ int main() {
         cout << "|\n|-------|--------------------------------------------------|\n";
     }
 
-    
+    //соберем список лучших вещей из оценки
+    cout << "List the best item:\n";
+    i = tableCost.size() - 1;
+    int j = maxBag - 1;
+    while ((i != 0)&&(j != 0)) {
+        if (resTable[i][j] == resTable[i - 1][j]) { i--; continue; }
+        cout << listItem[i] << endl;
+        for (const auto& k : tableCost[listItem[i]])
+            j -= k.first;
+        i--;
+    }
+
 
 }
